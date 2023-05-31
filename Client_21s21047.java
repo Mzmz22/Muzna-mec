8223714bb53582a1f97cefe80f4af457bbd81b13
Mzmz22 <135154505+Mzmz22@users.noreply.github.com>
2023-05-31 22:11:12 +0400

    Create Client_21s21047.java

---
+++ Client_21s21047.java
@@ -0,0 +1,9 @@
+public class MainClient_21s21047 {
+     public static void main (String args[]){
+      
+
+       Client_21s21047 A = new Client_21s21047();
+        Thread D = new Thread(A);
+        D.start();
+    } 
+}
